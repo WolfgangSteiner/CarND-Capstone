92a219770ad1d45c17dd801fdcdc7efa4a91f02c
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2017-09-04 19:05:52 +0200

    First iteration on dbw_node

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -35,6 +35,13 @@ class DBWNode(object):
     def __init__(self):
         rospy.init_node('dbw_node')
 
+        self.target_linear_velocity = None
+        self.target_angular_velocity = None
+        self.current_linear_velocity = None
+        self.current_angular_velocity = None
+        self.dbw_enabled = None
+
+
         vehicle_mass = rospy.get_param('~vehicle_mass', 1736.35)
         fuel_capacity = rospy.get_param('~fuel_capacity', 13.5)
         brake_deadband = rospy.get_param('~brake_deadband', .1)
@@ -54,25 +61,34 @@ class DBWNode(object):
                                          BrakeCmd, queue_size=1)
 
         # TODO: Create `TwistController` object
-        # self.controller = TwistController(<Arguments you wish to provide>)
+        self.controller = Controller()
 
         # TODO: Subscribe to all the topics you need to
+        rospy.Subscriber('/twist_cmd', TwistStamped, self.twist_cb)
+        rospy.Subscriber('/current_velocity', TwistStamped, self.velocity_cb)
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.dbw_cb)
 
         self.loop()
+
 
     def loop(self):
         rate = rospy.Rate(50) # 50Hz
         while not rospy.is_shutdown():
-            # TODO: Get predicted throttle, brake, and steering using `twist_controller`
-            # You should only publish the control commands if dbw is enabled
-            # throttle, brake, steering = self.controller.control(<proposed linear velocity>,
-            #                                                     <proposed angular velocity>,
-            #                                                     <current linear velocity>,
-            #                                                     <dbw status>,
-            #                                                     <any other argument you need>)
-            # if <dbw is enabled>:
-            #   self.publish(throttle, brake, steer)
+            if self.current_linear_velocity is None\
+            or self.target_linear_velocity is None:
+                continue
+
+            throttle, brake, steer = self.controller.control(
+                self.target_linear_velocity,
+                self.target_angular_velocity,
+                self.current_linear_velocity,
+                self.current_angular_velocity,
+                self.dbw_enabled)
+
+            self.publish(throttle, brake, -steer)
+
             rate.sleep()
+
 
     def publish(self, throttle, brake, steer):
         tcmd = ThrottleCmd()
@@ -91,6 +107,20 @@ class DBWNode(object):
         bcmd.pedal_cmd_type = BrakeCmd.CMD_TORQUE
         bcmd.pedal_cmd = brake
         self.brake_pub.publish(bcmd)
+
+
+    def twist_cb(self, msg):
+        self.target_linear_velocity = msg.twist.linear.x
+        self.target_angular_velocity = msg.twist.angular.z
+
+
+    def velocity_cb(self, msg):
+        self.current_linear_velocity = msg.twist.linear.x
+        self.current_angular_velocity = msg.twist.angular.z
+
+
+    def dbw_cb(self, msg):
+        self.dbw_enabled = msg.data
 
 
 if __name__ == '__main__':
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,3 +1,4 @@
+from pid import PID
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
@@ -5,10 +6,15 @@ ONE_MPH = 0.44704
 
 class Controller(object):
     def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+        self.linear_velocity_pid = PID(1.0, 0.1, 0.5, mn=0.0, mx=1.0)
+        self.angluar_velocity_pid = PID(10.0, 0.1, 0.5, mn=-0.43, mx=0.43)
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
-        # Return throttle, brake, steer
-        return 1., 0., 0.
+    def control(self,
+        target_linear_velocity, target_angular_velocity,
+        current_linear_velocity, current_angular_velocity,
+        dbw_enabled, **kwargs):
+        sample_time = 1 / 50.0
+        throttle = self.linear_velocity_pid.step(target_linear_velocity - current_linear_velocity, sample_time)
+        steer = self.angluar_velocity_pid.step(target_angular_velocity - current_angular_velocity, sample_time)
+
+        return throttle, 0.0, steer
